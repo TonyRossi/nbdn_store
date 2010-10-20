37fbb2fdc5187aea2d3ea14bad9ba21cc0253d5b
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-20 17:47:23 -0600

    Updating the spec

--- product/nothinbutdotnetstore/web/core/DefaultWebCommand.cs
+++ product/nothinbutdotnetstore/web/core/DefaultWebCommand.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace nothinbutdotnetstore.web.core
 {
     public class DefaultWebCommand : WebCommand
     {
-        
+        public void process(Request request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool can_handle(Request request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
