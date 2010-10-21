1856697d4b75a41ddfa976cfb3e2871de5f50aea
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-21 10:54:07 -0600

    Starting to refactor the web layer

--- product/nothinbutdotnetstore/domain/stubs/StubDepartmentRepository.cs
+++ product/nothinbutdotnetstore/domain/stubs/StubDepartmentRepository.cs
@@ -1,19 +1,25 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace nothinbutdotnetstore.domain.stubs
 {
-    public class StubDepartmentRepository : DepartmentRepository
+    public class StubDepartmentRepository : DepartmentRepository, ProductRepository
     {
         public IEnumerable<Department> get_all_the_main_departments()
         {
             return Enumerable.Range(1, 100).Select(x => new Department {name = x.ToString("Department 0")});
         }
 
+        public IEnumerable<Product> get_the_products_in(Department department)
+        {
+            return Enumerable.Range(1, 100).Select(x => new Product {});
+        }
+
         public IEnumerable<Department> get_the_departments_in(Department department)
         {
-            return Enumerable.Range(1, 100).Select(x => new Department {name = x.ToString("A Department In A Department 0")});
+            return
+                Enumerable.Range(1, 100).Select(
+                    x => new Department {name = x.ToString("A Department In A Department 0")});
         }
     }
 }
