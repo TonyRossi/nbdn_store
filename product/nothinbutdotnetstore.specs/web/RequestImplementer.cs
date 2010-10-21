d6e4b73e5ec0bc773288c98a8da4ffcc61297118
unknown <SChambers@.corporate.ncm>
2010-10-21 16:35:02 -0600

    a test

---
+++ product/nothinbutdotnetstore.specs/web/RequestImplementer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using nothinbutdotnetstore.web.core;
+
+namespace nothinbutdotnetstore.specs.web
+{
+    public class RequestImplementer : Request
+    {
+        public InputModel map<InputModel>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ product/nothinbutdotnetstore.specs/web/RequestSpecs.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.specs.utility;
+using nothinbutdotnetstore.web.core;
+using Rhino.Mocks;
+
+namespace nothinbutdotnetstore.specs.web
+{
+    public class RequestSpecs
+    {
+         public abstract class concern : Observes<Request,
+                                             RequestImplementer>
+         {
+        
+         }
+
+         [Subject(typeof(Request))]
+         public class when_asked_for_a_map : concern
+         {
+             Because b = () =>
+                 input_model = sut.map<InputModel>();
+
+             private It should_return_a_map_for_the_specified_input_model = () =>
+                 input_model.ShouldBeOfType(typeof (InputModel));
+
+             private static InputModel input_model;
+         }
+
+        public class InputModel
+        {
+            
+        }
+     }
+}
