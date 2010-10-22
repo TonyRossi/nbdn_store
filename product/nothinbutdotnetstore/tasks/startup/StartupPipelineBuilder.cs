49c40dbdb4c948aa89c4294e255aeae51f203e0f
neontapir <neontapir@gmail.com>
2010-10-22 16:24:11 -0600

    Adding support for first command

--- product/nothinbutdotnetstore/tasks/startup/StartupPipelineBuilder.cs
+++ product/nothinbutdotnetstore/tasks/startup/StartupPipelineBuilder.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+
 namespace nothinbutdotnetstore.tasks.startup
 {
     public class StartupPipelineBuilder
     {
-        
+        StartupCommandFactory factory;
+        IList<StartupCommand> all_commands;
+        StartupCommand the_first_command;
+
+        public StartupPipelineBuilder(StartupCommandFactory factory, IList<StartupCommand> allCommands, Type firstCommandType)
+        {
+            this.factory = factory;
+            all_commands = allCommands;
+            var startupCommand = factory.create_from(firstCommandType);
+            all_commands.Add(startupCommand);
+        }
     }
 }
