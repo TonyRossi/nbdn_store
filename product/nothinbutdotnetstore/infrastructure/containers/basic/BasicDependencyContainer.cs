b2ae3b8b462e28906d4cbd98516cc59af8538619
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-22 11:12:26 -0600

    Get the second test working

--- product/nothinbutdotnetstore/infrastructure/containers/basic/BasicDependencyContainer.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/BasicDependencyContainer.cs
@@ -1,20 +1,17 @@
-using System;
-
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class BasicDependencyContainer : DependencyContainer
     {
-        private readonly DependencyFactories _dependencyFactories;
+        DependencyFactories dependency_factories;
 
         public BasicDependencyContainer(DependencyFactories dependencyFactories)
         {
-            _dependencyFactories = dependencyFactories;
+            dependency_factories = dependencyFactories;
         }
 
         public Dependency an<Dependency>()
         {
-            DependencyFactory dependencyFactory = _dependencyFactories.get_factory_that_can_create(typeof (Dependency));
-            return (Dependency)dependencyFactory.create();
+            return (Dependency) dependency_factories.get_factory_that_can_create(typeof(Dependency)).create();
         }
     }
 }
