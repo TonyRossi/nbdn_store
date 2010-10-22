4f757a4f6c01c95d6751096ad84b3b77988fd93b
Adam Steffes <asteffes@graebel.com>
2010-10-22 15:11:35 -0600

    Got unit test working

--- product/nothinbutdotnetstore/infrastructure/containers/basic/AutomaticDependencyFactory.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/basic/AutomaticDependencyFactory.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class AutomaticDependencyFactory : DependencyFactory
     {
-        
+        private readonly DependencyContainer _dependencyContainer;
+        private readonly ConstructorSelectionStrategy _constructorSelectionStrategy;
+        private readonly Type _type;
+
+        public AutomaticDependencyFactory(DependencyContainer dependencyContainer, ConstructorSelectionStrategy constructorSelectionStrategy, Type type)
+        {
+            _dependencyContainer = dependencyContainer;
+            _constructorSelectionStrategy = constructorSelectionStrategy;
+            _type = type;
+        }
+
+        public object create()
+        {
+            return _constructorSelectionStrategy.get_applicable_constructor_on(_type)
+                .Invoke(_constructorSelectionStrategy.get_applicable_constructor_on(_type)
+                .GetParameters().Select(info => _dependencyContainer.an(info.ParameterType)).ToArray());
+        }
     }
 }
