d22035f195641554c79d15ab5c8cca74045203ab
neontapir <neontapir@gmail.com>
2010-10-22 09:21:28 -0600

    For the win

--- product/nothinbutdotnetstore/infrastructure/logging/basic/TextWriterLogger.cs
+++ product/nothinbutdotnetstore/infrastructure/logging/basic/TextWriterLogger.cs
@@ -1,7 +1,20 @@
+using System;
+using System.IO;
+
 namespace nothinbutdotnetstore.infrastructure.logging.basic
 {
     public class TextWriterLogger : Logger
     {
-        
+        private TextWriter writer;
+
+        public TextWriterLogger(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void informational(string message)
+        {
+            writer.WriteLine(message);
+        }
     }
 }
