fb6f8183d792c8169ed936b821b6b3032c863ae4
Jeremy Paden <jpaden@graebel.com>
2010-10-22 09:39:35 -0600

    Added log4net implementation

--- product/nothinbutdotnetstore/infrastructure/logging/log4net/Log4NetLog.cs
+++ product/nothinbutdotnetstore/infrastructure/logging/log4net/Log4NetLog.cs
@@ -1,12 +1,20 @@
 using System;
+using log4net;
 
 namespace nothinbutdotnetstore.infrastructure.logging.log4net
 {
     public class Log4NetLog : Logger
     {
+        private readonly ILog _underlyingLogger;
+
+        public Log4NetLog(ILog underlying_logger)
+        {
+            _underlyingLogger = underlying_logger;
+        }
+
         public void informational(string message)
         {
-            throw new NotImplementedException();
+            _underlyingLogger.Info(message);
         }
     }
 }
