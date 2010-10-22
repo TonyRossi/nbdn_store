9521928596f5a7ae1fc4efd4bbc55c962d83e0c2
unknown <Ollie@.(none)>
2010-10-22 11:56:39 -0600

    Fixed FINALLY

---
+++ product/nothinbutdotnetstore.specs/infrastructure/DependencyFactoriesSpecs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+using Machine.Specifications.DevelopWithPassion.Rhino;
+using nothinbutdotnetstore.infrastructure.containers;
+using nothinbutdotnetstore.infrastructure.containers.basic;
+
+namespace nothinbutdotnetstore.specs.infrastructure
+{
+    public class DependencyFactoriesSpecs
+    {
+        public abstract class concern : Observes <DependencyFactories>
+        {
+        }
+
+        [Subject(typeof(Container))]
+        public class when_providing_a_factory : concern
+        {
+            Establish c = () =>
+                              {
+                                  dependency_type = typeof (int);
+                              };
+
+            private Because b = () =>
+                               result = sut.get_factory_that_can_create(dependency_type);
+
+            private It should_return_a_factory_that_can_create_the_type = () =>
+                             result.type_that_can_be_created.ShouldBeOfType(dependency_type);
+
+            static Type dependency_type;
+            static DependencyFactory result;
+        }
+    }
+}
